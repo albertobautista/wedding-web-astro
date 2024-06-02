fec527acbd177ef8afde582a3149848c6e2c28bd
Alberto Bautista <dachb101@gmail.com>
2024-06-01 21:39:48 -0600

    Add colors to tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        atomic: ["Dancing Script Variable", "cursive"],
+      },
+      colors: {
+        primary: "#654b2e",
+        secondary: "#FFA500",
+        tertiary: "#FF6347",
+        quaternary: "#FF4500",
+        quinary: "#FF0000",
+      },
+    },
+  },
+  plugins: [],
+};
